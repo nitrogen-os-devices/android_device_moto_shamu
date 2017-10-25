43df1efcf6137450e86f5031709949a865fc5ba0
Christian Oder <myself5@carbonrom.org>
2017-10-25 21:27:38 +0200

    Carbonify
    Change-Id: Ie592e5cc976aff9082dd26484cf590bf10db378c

--- carbon_shamu.mk
+++ carbon_shamu.mk
@@ -2,20 +2,26 @@
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+# Inherit Carbon product configuration
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-carbon
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_shamu
+PRODUCT_NAME := carbon_shamu
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
-
+TARGET_MANUFACTURER := Google
 TARGET_VENDOR := moto
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.carbon.maintainer="Myself5/iamr00t/PatrickC"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
